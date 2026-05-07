2f2834831b456c8b6e38ef885eee2e387b2af700
esdraspresidente <esdraspresidente@gmail.com>
2026-05-07 08:48:58 -0300

    feat: PWA - instalar como app no celular

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'mind-tracker-v1';
+const ASSETS = [
+  '/mind-tracker/',
+  '/mind-tracker/index.html',
+  '/mind-tracker/manifest.json',
+  '/mind-tracker/icon-192.png',
+  '/mind-tracker/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Sempre busca online primeiro, cai no cache se offline
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
