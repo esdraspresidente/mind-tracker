dc85b65d406daad47d606f467b56acff60c46c51
esdraspresidente <esdraspresidente@gmail.com>
2026-05-09 07:43:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => self.clients.claim()));
 });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
